103de128efd41a8d7fa9235b97790fa680a5ba33
Julien Lesieur <lesieur.julien.pro@protonmail.com>
2017-09-27 15:48:21 +0200

    Laid folder structure out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+//////////Components\\\\\\\\\\
 import { AppComponent } from './app.component';
+//###############Modules folder###############\\
+//Login
+import { LoginComponent } from '../modules/login/components/login.component';
+import { EmailInputComponent } from '../modules/login/components/email-input.component';
+import { PasswordInputComponent } from '../modules/login/components/password-input.component';
+//Register
+
+
+//Shared folder
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent, //Shared
+    FooterComponent, //Shared
+    LoginComponent, //Login
+    EmailInputComponent, //Login
+    PasswordInputComponent, FooterComponent //Login
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/modules/login/components/password-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-input',
+  templateUrl: '../views/password-input.component.html',
+  // styleUrls: ['./login.component.css']
+})
+export class PasswordInputComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
